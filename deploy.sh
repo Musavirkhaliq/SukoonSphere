a3eba09546911e02f8f3d626fb63b7a7d5064830
root <root@sukoon.sphere>
2025-02-17 09:38:20 +0000

    Deploy to main

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,29 @@
 # Navigate to the project directory
 cd /root/Working_Directory/SukoonSphere
 
+
+#stash
+git stash
+
 # Pull the latest changes from GitHub
 git pull origin main
+
+# pop stash
+git stash pop
 
 # Install dependencies
 npm install
 
+cd /root/Working_Directory/SukoonSphere/client
+
+npm run build
+
+rsync -a /root/Working_Directory/SukoonSphere/client/dist/ /root/Working_Directory/SukoonSphere/public/
+
+cd /root/Working_Directory/SukoonSphere
 # Restart the application using PM2
 pm2 restart server.js
+
+# node server.js
+
+
---
+++ public/server.js
@@ -0,0 +1,135 @@
+import "express-async-errors";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import http from "http"; // Import http for Socket.IO
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import cors from "cors";
+import { Server } from "socket.io"; // Import Socket.IO
+
+// Routers
+import AuthRouter from "./routes/authRouter.js";
+import PostRouter from "./routes/postRouter.js";
+import UserRouter from "./routes/userRouter.js";
+import QaSectionRouter from "./routes/qaRouter.js";
+import ArticleRouter from "./routes/articleRouter.js";
+import VideoRouter from "./routes/videoRouter.js";
+import PodcastRouter from "./routes/podcastRouter.js";
+import GalleryRouter from "./routes/galleryRouter.js";
+import NotificationRouter from "./routes/notificationRouter.js"; 
+import MessageRouter from "./routes/messageRouter.js";
+import ChatRouter from "./routes/chatRouter.js";
+
+// models
+import Notification from "./models/notifications/postNotificationModel.js";
+
+// Public
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Middleware
+import errorHandlerMiddleware from "./middleware/errorhandlerMiddleware.js";
+
+// Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+// Initialize Express App
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const server = http.createServer(app); // Create server instance
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Allow your front-end origin
+    methods: ["GET", "POST"],
+    credentials: true, // Allow credentials if needed
+  },
+});
+
+// Middleware Setup
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+  pingInterval: 25000, // Send ping every 25s
+  pingTimeout: 60000,  // Disconnect if no response in 60s
+
+}));
+
+// API Routes
+app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/posts", PostRouter);
+app.use("/api/v1/qa-section", QaSectionRouter);
+app.use("/api/v1/articles", ArticleRouter);
+app.use("/api/v1/gallery", GalleryRouter);
+app.use("/api/v1/videos", VideoRouter);
+app.use("/api/v1/podcasts", PodcastRouter);
+app.use("/api/v1/notifications", NotificationRouter); 
+app.use("/api/v1/messages", MessageRouter);
+app.use("/api/v1/chats", ChatRouter);
+
+// Serve Static Files
+app.use("/public", express.static(path.resolve(__dirname, "./public")));
+
+// Fallback Route
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
+// Not Found Handler
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "route not found " });
+});
+
+// Error Handling Middleware
+app.use(errorHandlerMiddleware);
+
+// Socket.IO Connection
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Join a room based on user ID
+  socket.on('join', async (userId) => {
+      socket.join(userId);
+      console.log(`User ${userId} joined room`);
+
+      // Fetch the total number of notifications for the user
+      const notificationCount = await Notification.countDocuments({userId});
+      io.to(userId).emit('notificationCount', notificationCount);
+  });
+
+  socket.on('disconnect', () => {
+      console.log('User disconnected');
+  });
+});
+
+// Start Server
+const PORT = process.env.PORT || 5100; // Default to 5100 if PORT is not set
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+})();
+
+export { io }; // Export the io instance for use in controllers
